2aa74d64bc1426c3968b73083e9c2b7ac8a655ee
imtiyaz-hirani <i.hirani@techskillsit.com>
2023-02-02 11:38:30 +0530

    array ops-2

--- uiapp/javascript/arraysApp.js
+++ uiapp/javascript/arraysApp.js
@@ -8,6 +8,8 @@ function fetchAllElements(){
 
 function iterateArray(){
     arry.forEach(e=>console.log(e)); 
+    joinArray();
+  //  fillArray();
 }
 
 function sortArray(sort){
@@ -16,8 +18,39 @@ function sortArray(sort){
         temp = arry.sort((a,b)=>a-b);
     else
         temp = arry.sort((a,b)=>b-a);  
-          
+
     document.getElementById('allelements').innerHTML='<br><strong>' + temp + '</strong>';
 
 }
 
+function filterArray(str){
+    let temp =[];
+    let num = Number(document.getElementById('num').value);
+    if(str == 'SMALL')
+        temp = arry.filter(e=>e<num);
+    else
+        temp = arry.filter(e=>e>num);
+    
+    document.getElementById('allelements').innerHTML='<br><strong>' + temp + '</strong>';
+    document.getElementById('arrayLength').innerHTML='Array Length = ' + temp.length;
+}
+
+function joinArray(){ //Generates String of Array 
+    let temp = arry.join('-');
+    console.log(temp);
+    //let tempArry = temp.split('-'); : gives array back from String 
+}
+
+function fillArray(){
+    let temp = arry.fill(5);
+    console.log(temp);
+}
+
+function searchElement(){
+    let num = Number(document.getElementById('snum').value);
+    if(arry.find(e=>e == num) == undefined)
+        document.getElementById('findResult').innerHTML='Element ' + num + ' is NOT Found!!';
+    else
+        document.getElementById('findResult').innerHTML='Element ' + num + ' is Found!!';
+}
+/* Predicate: given an expression that returns a boolean */
