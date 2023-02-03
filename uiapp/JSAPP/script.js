0ed92246cea51dcf24ff5cc996283083aa2d5d82
imtiyaz-hirani <i.hirani@techskillsit.com>
2023-02-03 10:30:08 +0530

    async await in fetch for all posts

--- uiapp/JSAPP/script.js
+++ uiapp/JSAPP/script.js
@@ -1,20 +1,20 @@
-
-function getAllPosts(){
-    let posts=[];
-
-    fetch('https://jsonplaceholder.typicode.com/posts') 
-        .then(response=>response.json())                
-        .then(data=> {
-            data.forEach(p => {
-                posts.push(p);
-            });
-        });     
-        
+let posts=[];
+async function getAllPosts(){
+        let response  = await fetch('https://jsonplaceholder.typicode.com/posts'); 
+        let data = await response.json(); 
+        posts = data;
         /*
-        1. Call the API
+        1. Call the API and save it in response
         2. convert response to JSON
         3. assign data of API to posts
-        */
-         
-        console.log(posts);
+        */  
+       let str='';
+        posts.forEach(p => {
+            str = str + '<div class="jumbotron"><h1 class="display-6">' + 
+            p.title + '</h1><p class="lead">' + p.body + '</p><hr class="my-4"></div>';
+        });
+        
+        document.getElementById('post').innerHTML=str;
+     
 }
+
