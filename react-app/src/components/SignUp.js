92ac120b4f0f2a1dee770dafcce6eef1360a58b0
imtiyaz-hirani <i.hirani@techskillsit.com>
2023-02-10 10:52:12 +0530

    form processing with validation

--- react-app/src/components/SignUp.js
+++ react-app/src/components/SignUp.js
@@ -10,7 +10,8 @@ export default class SignUp  extends Component{
                     name: '',
                     email: '',
                     password: ''
-                }
+                },
+                errors: {}
         }
     }
 
@@ -25,18 +26,21 @@ export default class SignUp  extends Component{
                         name="name"
                         value={this.state.user.name}
                         onChange={this.changeHandler} />
+                        <span style={{ color : 'red'}}>{this.state.errors['name']}</span>
                 <br /><br />
                 <label>Enter Email: </label>
                 <input type="text" 
                         name="email"
                         value={this.state.user.email}
                         onChange={this.changeHandler} />
+                        <span style={{ color : 'red'}}>{this.state.errors['email']}</span>
                 <br /><br />
                 <label>Enter password: </label>
                 <input type="password" 
                         name="password"
                         value={this.state.user.password}
                         onChange={this.changeHandler} />
+                        <span style={{ color : 'red'}}>{this.state.errors['password']}</span>
                 <br /><br />
                 <button onClick={this.onSignUp}>Sign Up</button>
             </div>
@@ -52,6 +56,42 @@ export default class SignUp  extends Component{
     }
 
     onSignUp = ()=>{
-        console.log(this.state.user)
+        /* Validate User inputs */
+        if(this.handleValidation()){
+            console.log(this.state.user);
+            /* Call the API */
+        }
+        else{
+            /* Display error messages */
+            console.log('validation not passed..');
+             
+        }
+        
+    }
+
+    handleValidation(){
+        let name = this.state.user.name;
+        let email = this.state.user.email; 
+        let password = this.state.user.password; 
+        let tempErrors={}
+        let formValid = true; 
+        if(!name){ //If name is not given
+            formValid = false;
+            tempErrors['name']='Name cannot be empty';
+        }
+        if(!email){ //If email is not given
+            formValid = false;
+            tempErrors['email']='Email cannot be empty';
+        }
+        if(!password){ //If password is not given
+            formValid = false;
+            tempErrors['password']='Password cannot be empty';
+        }
+
+        this.setState({
+            errors: tempErrors
+        });
+
+        return formValid; 
     }
 }
