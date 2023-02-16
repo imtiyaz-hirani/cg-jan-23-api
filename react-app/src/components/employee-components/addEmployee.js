18643dd0064ce4da0af66c43d80c17a1145b2904
imtiyaz-hirani <i.hirani@techskillsit.com>
2023-02-16 14:48:57 +0530

    post employee done

--- react-app/src/components/employee-components/addEmployee.js
+++ react-app/src/components/employee-components/addEmployee.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { Component } from "react";
 import { connect } from "react-redux";
 import {listDepartment,addDepartment} from '../../store/action/department';
@@ -15,11 +16,9 @@ export class AddEmployee extends Component{
                 joiningDate: '',
                 gender: '',
                 departmentID: '',
-                user: {
-                    username: '',
-                    password: '',
-                    role: 'EMPLOYEE'
-                }
+                username: '',
+                password: '',
+                role: 'EMPLOYEE'  
             },
             errors: {},
             msg: '',
@@ -72,7 +71,8 @@ export class AddEmployee extends Component{
                     <label>Select Gender: </label>
                     <select name="gender" 
                             value={this.state.employee.gender} 
-                            onChange={this.changeHandler} > 
+                            onChange={this.changeHandler} >
+                        <option key={0} value="">--Select Gender--</option>
                         <option key={1} value="MALE">MALE</option>
                         <option key={2} value="FEMALE">FEMALE</option>
                     </select>
@@ -82,7 +82,9 @@ export class AddEmployee extends Component{
                     <select name="departmentID" 
                             value={this.state.employee.departmentID} 
                             onChange={this.changeHandler} > 
+                             <option key={0} value="">--Select Department--</option>
                         {  this.props.dept.list.map(d=>(
+                           
                             <option key={d.id} value={d.id}>{d.name}</option>
                             
                         ))  }
@@ -90,6 +92,21 @@ export class AddEmployee extends Component{
                     </select>
                     <span style={{ color : 'red'}}>{this.state.errors['departmentId']}</span>
                     <br /><br />
+                    
+                    <label>Username: </label>
+                    <input type="text" 
+                            name="username"
+                            value={this.state.employee.username}
+                            onChange={this.changeHandler} />
+                            <span style={{ color : 'red'}}>{this.state.errors['username']}</span>
+                            <br /><br />
+                    <label>Password: </label>
+                    <input type="password" 
+                            name="password"
+                            value={this.state.employee.password}
+                            onChange={this.changeHandler} />
+                            <span style={{ color : 'red'}}>{this.state.errors['password']}</span>
+                            <br /><br />
                     <button onClick={this.onAdd} className="btn btn-primary">Add Employee</button>
                 </p>
                  
@@ -128,6 +145,8 @@ handleValidation(){
     let joiningDate = this.state.employee.joiningDate;
     let gender = this.state.employee.gender;
     let departmentId = this.state.employee.departmentID;
+    let username = this.state.employee.username;
+    let password = this.state.employee.password;
      
     let tempErrors={}
     let formValid = true; 
@@ -156,6 +175,14 @@ handleValidation(){
         formValid = false;
         tempErrors['departmentId']='Please select department ID';
     }
+    if(!username){ //If name is not given
+        formValid = false;
+        tempErrors['username']='Please enter username';
+    }
+    if(!password){ //If name is not given
+        formValid = false;
+        tempErrors['password']='Please enter password';
+    }
     this.setState({
         errors: tempErrors
     });
@@ -163,8 +190,33 @@ handleValidation(){
     return formValid; 
 }
 
-async postEmployee(employee){
-   console.log('call api ')
+async postEmployee(e){
+    let emp = {
+        name: e.name,
+        city: e.city,
+        salary: e.salary,
+        joiningDate: e.joiningDate,
+        gender: e.gender,
+        user: {
+            username: e.username,
+            password: e.password,
+            role: 'EMPLOYEE'
+        }       
+    }
+    try {
+        const response = axios.post("http://localhost:8585/api/employee/add/" + e.departmentID, emp);
+        const data = (await response).data;
+        console.log('API success');
+        console.log(data);
+        this.setState({
+            msg: "Employee Registered"
+        })
+        this.props.addEmployee(data);
+      } catch (error) {
+        this.setState({
+            msg: 'Operation Failed'
+        })
+      }
 }
 }
 
